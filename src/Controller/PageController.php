bb692ea4b0065ccd8c34d93903bf2f5ec75d4761
Mathias Le Faouder <mathias.lefaouder@precom.fr>
2022-04-15 08:47:04 +0200

    start integration

---
+++ src/Controller/PageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    final public function index(): Response
+    {
+        return $this->render( 'pages/index.html.twig');
+    }
+
+    #[Route('/mentions', name: 'app_mentions')]
+    final public function mentions(): Response
+    {
+        return $this->render( 'pages/mentions.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    final public function contact(): Response
+    {
+        return $this->render( 'pages/contact.html.twig');
+    }
+}

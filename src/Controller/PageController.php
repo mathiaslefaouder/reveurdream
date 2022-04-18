28082ac96d2c677f348235ba552949310d55ab7e
Mathias Le Faouder <mathias.lefaouder@precom.fr>
2022-04-18 16:16:47 +0200

    ajout data

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use App\Entity\Theme;
+use App\Repository\CategoryRepository;
+use App\Repository\DreamRepository;
 use App\Repository\ThemeRepository;
 use App\Service\LunaryPhase;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,9 +17,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    final public function index(): Response
+    final public function index(DreamRepository $dreamRepository, CategoryRepository $categoryRepository, ThemeRepository $themeRepository): Response
     {
-        return $this->render( 'pages/index.html.twig');
+        return $this->render( 'pages/index.html.twig',[
+            'dreams' => $dreamRepository->findAll(),
+            'categories' => $categoryRepository->findAll(),
+            'themes' => $themeRepository->findAll(),
+        ]);
     }
 
     #[Route('/mentions', name: 'app_mentions')]

29cb74a48739a43a98a770c10fe4b95f916dee5b
Mathias Le Faouder <mathias.lefaouder@precom.fr>
2022-06-30 14:33:22 +0200

    lunar

--- src/Service/LunaryPhaseService.php
+++ src/Service/LunaryPhaseService.php
@@ -38,24 +38,26 @@ class LunaryPhaseService
             'texturize' => false,
         ];
 
-        try {
             $response = $this->client->request('GET', 'https://www.icalendar37.net/lunar/api/', [
                 'query' => $conf,
                 'verify_host' => false,
             ]);
-            $data = json_decode($response->getContent(), true);
-            $data = $data['phase'][(date('j'))];
-        } catch (\Exception $e) {
-            $data =   ["phaseName" => "Croissant",
-                  "isPhaseLimit" => false,
-                  "lighting" => 1.5438248430572,
-                  "svg" => '<svg width="24" height="24" viewBox="0 0 100 100"><g><circle cx="50" cy="50" r="49" stroke="none"  fill="rgb(6,14,37)"/><path d="M 50 1 A 49,49 0 0,1 49,99 A 48 ▶',
-                  "svgMini" => false,
-                  "timeEvent" => false,
-                  "dis" => 405272.2379007,
-                  "dayWeek" => 3,
-                  "npWidget" => "Croissant (1%)"];
-        }
+            if (200 == $response->getStatusCode()){
+                $data = json_decode($response->getContent(), true);
+                $data = $data['phase'][(date('j'))];
+            }else{
+                $data =   ["phaseName" => "Croissant",
+                    "isPhaseLimit" => false,
+                    "lighting" => 1.5438248430572,
+                    "svg" => '<svg width="24" height="24" viewBox="0 0 100 100"><g><circle cx="50" cy="50" r="49" stroke="none"  fill="rgb(6,14,37)"/><path d="M 50 1 A 49,49 0 0,1 49,99 A 48.02,49 0 1,0 50,1" stroke-width="0" stroke="none" fill="rgb(85,192,243)" /><a xlink:href="https://www.icalendar37.net/lunar/app/" rel="noopener noreferrer" target="_blank"><circle cx="50" cy="50" r="49" style="pointer-events:all;cursor:pointer" stroke-width="0"   fill="transparent" /></a></g></svg>',
+                    "svgMini" => false,
+                    "timeEvent" => false,
+                    "dis" => 405272.2379007,
+                    "dayWeek" => 3,
+                    "npWidget" => "Croissant (1%)"];
+
+            }
+
 
         return $data;
     }

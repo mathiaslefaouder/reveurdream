22ee3fa82a8cc5c5f6832c512c4909c3e660e7cd
Mathias Le Faouder <mathias.lefaouder@precom.fr>
2022-06-30 14:27:47 +0200

    trad + catch

--- src/Service/LunaryPhaseService.php
+++ src/Service/LunaryPhaseService.php
@@ -43,11 +43,20 @@ class LunaryPhaseService
                 'query' => $conf,
                 'verify_host' => false,
             ]);
-        } catch (TransportExceptionInterface $e) {
-            throw $e;
+            $data = json_decode($response->getContent(), true);
+            $data = $data['phase'][(date('j'))];
+        } catch (\Exception $e) {
+            $data =   ["phaseName" => "Croissant",
+                  "isPhaseLimit" => false,
+                  "lighting" => 1.5438248430572,
+                  "svg" => '<svg width="24" height="24" viewBox="0 0 100 100"><g><circle cx="50" cy="50" r="49" stroke="none"  fill="rgb(6,14,37)"/><path d="M 50 1 A 49,49 0 0,1 49,99 A 48 ▶',
+                  "svgMini" => false,
+                  "timeEvent" => false,
+                  "dis" => 405272.2379007,
+                  "dayWeek" => 3,
+                  "npWidget" => "Croissant (1%)"];
         }
-        $data = json_decode($response->getContent(), true);
 
-        return $data['phase'][(date('j'))];
+        return $data;
     }
 }
